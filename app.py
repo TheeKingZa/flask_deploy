9ae1ec316cdc8e13b98fabd2455b52e659e3befb
Pule Mathikha <122857549+TheeKingZa@users.noreply.github.com>
2024-05-24 02:54:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,14 @@
-#!/usr/bin/env python3
-from flask import (
-  Flask,
-  render_template,
-  request,
-  redirect,
-  abort
-)
+from flask import Flask, render_template
 
-# Create an instance of the Flask class
 app = Flask(__name__)
 
-# Define routes and their corresponding views
-@app.route('/')
+@app.route('/index')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    return f'User: {username}'
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        # Check username and password
-        return 'Logged in successfully!'
-    return render_template('login.html')
-
-
-@app.route('/redirect')
-def redirect_example():
-    return redirect('/')
-
-@app.route('/error')
-def error_example():
-    abort(404)
-
+@app.route('/app')
+def app_page():
+    return render_template('app.html')
 
 if __name__ == '__main__':
-    # Run the Flask application
     app.run(debug=True)
