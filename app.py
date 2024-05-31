67c8c0221a442a295bddea54c149cd2af297e5ee
Pule <proceedingmc@gmail.com>
2024-05-31 22:41:18 +0200

    setting up the login and the sign up form_ prepering for the functionalities

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def login():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 
 @app.route('/home')
-def app_page():
-    return render_template('app.html')
+def home():
+    return render_template('home.html')
 
 
 @app.route('/about')
